d6ac5c363b75590a32cd49f29a9dbea8db236d41
MichaelGKing91 <michaelking@quickenloans.com>
2021-03-22 14:31:24 -0400

    first commit

---
+++ Lab 3.1/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Lab_3._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to lab 3.1!");        }
+    }
+}
